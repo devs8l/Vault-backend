12dee9542c77f47f6eea446d0229a470587ab343
devs8l <developer.stellar8@gmail.com>
2025-06-24 23:16:56 +0530

    first commit

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // MongoDB Connection
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
